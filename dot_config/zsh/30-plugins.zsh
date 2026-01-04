9cad022bdb2dd5cb0300ed228488759f0927d576
Ryan Wolfe <rn.wolfe@gmail.com>
2026-01-04 22:38:35 +0000

    feat: add powerlevel10k prompt

--- dot_config/zsh/30-plugins.zsh
+++ dot_config/zsh/30-plugins.zsh
@@ -4,6 +4,9 @@ ZINIT_HOME="${XDG_DATA_HOME:-$HOME/.local/share}/zinit/zinit.git"
 if [ -f "$ZINIT_HOME/zinit.zsh" ]; then
   source "$ZINIT_HOME/zinit.zsh"
 
+  zinit ice depth=1
+  zinit light romkatv/powerlevel10k
+
   zinit light zsh-users/zsh-autosuggestions
   zinit light zsh-users/zsh-syntax-highlighting
   zinit light Aloxaf/fzf-tab
